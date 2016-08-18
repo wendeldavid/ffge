fec8f70ad1930c2072e1f8424a3ba29939c5e831
Wendel.Przygoda <wendel.przygoda@senior.com.br>
2016-08-18 13:56:57 -0300

    ajuste minuto da ofensa close #4

--- WebContent/js/controller.js
+++ WebContent/js/controller.js
@@ -11,7 +11,7 @@ app.controller('FFCtrl', [ '$scope', '$http', 'ModalService', function($scope, $
     $scope.ff.grosserias = [];
 
     $scope.load = function() {
-        $http.get('http://pcbnu007999:8188/ffge/Servlet').then(function(success) {
+        $http.get('http://pcbnu007999:8888/ffge/Servlet').then(function(success) {
             $scope.ff = success.data.ff;
             $scope.ff.grosserias = success.data.ff.grosserias.reverse();
         }, function(error) {
@@ -29,7 +29,7 @@ app.controller('FFCtrl', [ '$scope', '$http', 'ModalService', function($scope, $
             modal.close.then(function(params) {
                 console.log(params);
                 if (!!params) {
-                    $http.put('http://pcbnu007999:8188/ffge/Servlet', params).then(function(sucess) {
+                    $http.put('http://pcbnu007999:8888/ffge/Servlet', params).then(function(sucess) {
                         $scope.load();
                     }, function(error) {
                         console.log(error);
