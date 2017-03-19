ffdf2ae42e22650115e0602eb5b0a5e40679d160
wendeldavid <wendel.hades@gmail.com>
2017-03-19 18:15:51 -0300

    ajustes gerais de base do firebase

--- firebase/public/js/controller.js
+++ firebase/public/js/controller.js
@@ -32,28 +32,53 @@ app.controller('FFCtrl', [
 
 				var database = firebase.database();
 
-				var recorde = firebase.database().ref('recorde');
-				recorde.on('value', function(snapshot) {
-					$scope.$apply(function() {
-						$scope.ff.recorde = snapshot.val();
-					});
-				});
-
-				var dias = firebase.database().ref('dias');
-				dias.on('value', function(snapshot) {
-					$scope.$apply(function() {
-						$scope.ff.dias = snapshot.val();
-					});
-				});
-
 				var grosserias = firebase.database().ref('grosserias');
 				grosserias.on('value', function(snapshot) {
 					$scope.$apply(function() {
 						$scope.ff.grosserias = snapshot.val().reverse();
+						
+						$scope.calcCurrentStatus();
+						$scope.calcRecord();
 					});
 				});
 			}
+			
+			$scope.calcCurrentStatus = function(){
+				var lastDate = $scope.ff.grosserias[0].data;
+				var end = new Date(lastDate.date.year, lastDate.date.month, lastDate.date.day, lastDate.time.hour, lastDate.time.minute, lastDate.time.second, lastDate.time.nano);
+				var now = new Date();
 
+				$scope.ff.dias = Math.round(end.getDay() - now.getDay());
+			}
+
+			$scope.calcRecord = function(){
+				var begin = $scope.ff.grosserias[0]; 
+				var end = $scope.ff.grosserias[1];
+				var record = $scope.diffDays(begin.data, end.data);
+				for (var i = 1; i < $scope.ff.grosserias.length; i++){
+					begin = $scope.ff.grosserias[i];
+					end = $scope.ff.grosserias[i + 1];
+
+					if(!!end){
+						var diff = $scope.diffDays(begin.data, end.data);
+						if(diff > record){
+							record = diff;
+						}
+					}
+				}
+				
+				$scope.ff.recorde = record;
+			}
+			
+			$scope.diffDays = function(begin, end){
+				var oneDay = 24*60*60*1000; // hours*minutes*seconds*milliseconds
+				
+				var beginDate = new Date(begin.date.year, begin.date.month, begin.date.day, begin.time.hour, begin.time.minute, begin.time.second);
+				var endDate = new Date(end.date.year, end.date.month, end.date.day, end.time.hour, end.time.minute, end.time.second);
+				
+				return Math.round(Math.abs((beginDate.getTime() - endDate.getTime())/(oneDay)));
+			}
+			
 			$scope.getPage = function(currentPage) {
 				return $scope.ff.grosserias.slice(
 						parseInt(currentPage + '0') - 10,
